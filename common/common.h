5a645098e826d26829d881d5673fd2436e0aa277
sunus lee <sunuslee@gmail.com>
2011-04-22 16:44:31 +0800

    ADD TWO DEBUG MRACOS IN common.h

--- common/common.h
+++ common/common.h
@@ -24,3 +24,37 @@ do                                              \
                 printf("In calloc"              \
                         "Out Of Memory\n");     \
 }while(0)                                       
+
+
+#define DEBUG(on, val)                                                  \
+do                                                                      \
+{                                                                       \
+        if(on)                                                          \
+                {                                                       \
+                printf("Debug Info :%s , %d , val = %08x\n",            \
+                                __func__,__LINE__,(uint32_t)(val));     \
+                while(getchar() != 'c')                                 \
+                       /* Press 'C' to continue */        ;             \
+                }                                                       \
+}while(0)
+
+/* SAMPLES: 
+ * DEBUG_ARGS(1,val,"I ALSO WANT TO CHECK val2 = %d\n",val2);
+ * Or
+ * DEBUG_ARGS(1,val,"I ALSO WANT TO ADD THIS INFO");
+ */
+#define DEBUG_ARGS(on, val,info,...)                                    \
+do                                                                      \
+{                                                                       \
+        if(on)                                                          \
+                {                                                       \
+                printf("Debug Info :%s , %d , val = %08x\n",            \
+                                __func__,__LINE__,(uint32_t)(val));     \
+                printf(info,##__VA_ARGS__);                             \
+                        /* ##__VA_ARGS__ is supported by GCC ONLY!*/    \
+                while(getchar() != 'c')                                 \
+                        /* Press 'C' to continue */        ;            \
+                }                                                       \
+}while(0)
+
+
