66f63c56a8a1ab1cff610ea1e35a680a8de9fdd7
sunus lee <sunuslee@gmail.com>
2011-04-28 21:05:14 +0800

    fix some bugs

--- rbtree/rbtree.c
+++ rbtree/rbtree.c
@@ -1,100 +1,6 @@
 #include "rbtree.h"
 #include "queue/queue.h"
-/*      Use VAL_Int Or VAL_DOUBLE as 'type' parameter      */
-#define VAL_INT                 i_val
-#define VAL_DOUBLE              d_val
-
-/*      do NOT use those two!!    */
-#define CMP_INT 0x4
-#define CMP_DOUBLE 0x12
-
-#define COLOR(rbt)              ((struct _rbtree *)rbt)->attr.color
-#define LEFT(rbt)               ((struct _rbtree *)rbt)->attr.left
-#define RIGHT(rbt)              ((struct _rbtree *)rbt)->attr.right
-#define P(rbt)                  ((struct _rbtree *)rbt)->attr.parent
-#define FREE_ADDR(rbt)          ((struct _rbtree *)rbt)->attr.free_addr
-#define RBT_DATA(rbt)           (((struct _rbtree *)rbt)->pdata)
-#define UNCLE(rbt)              (LEFT(P(P(rbt))) == P(rbt) ? (RIGHT(P(P(rbt)))) : (LEFT(P(P(rbt)))))
-#define RIGHT_UNCLE(rbt)        (RIGHT(P(P(rbt))))
-#define LEFT_UNCLE(rbt)         (LEFT(P(P(rbt))))
-#define IS_UNCLE_LEFT(rbt)      (LEFT(P(P(rbt))) == P(rbt) ? FALSE : TRUE)
-#define IS_PARENT_LEFT(rbt)     (LEFT(P(P(rbt))) == P(rbt) ? TRUE : FALSE)
-#define IS_NODE_LEFT(rbt)       (LEFT(P(rbt)) == rbt ? TRUE : FALSE)
-#define SIBLING(rbt)            ((IS_NODE_LEFT(rbt)) ? RIGHT(P(rbt)) : LEFT(P(rbt)))
-
-#define CMPTYPE(rbt)            ((struct _rbtree *)rbt)->cmp.type
-#define CMP(a,op,c,type)        ((((struct _rbtree *)a)->cmp.val.type op ((struct _rbtree *)c)->cmp.val.type) ? 1 : 0)
-
-/*      Some Value Operations Marcos */
-/*      VAL(brt) use to do the comparision ONLY e.g : if(VAL(rbt1) > VAL(rbt2)) ......  */
-#define VAL(rbt)                ({CMPTYPE(rbt) == CMP_INT ?                             \
-                                ((struct _rbtree *)rbt)->cmp.val.i_val :                \
-                                ((struct _rbtree *)rbt)->cmp.val.d_val;})
-/*      GET_VAL use to assign dst with rbt's value,Use VAL_Int Or VAL_DOUBLE 3rd parameterOnly! */
-#define GET_VAL(dst, rbt,type)      dst = ((struct _rbtree *)rbt)->cmp.val.type
-
-
-#define SET_VAL_WITH_TYPE(rbt, val, type)  ((struct _rbtree *)rbt)->cmp.val.type = (val)
-#define SET_VAL(rbt, Val)                                                               \
-do                                                                                      \
-{                                                                                       \
-        if((CMPTYPE(rbt)) == CMP_INT)                                                   \
-                ((struct _rbtree *)rbt)->cmp.val.i_val = (Val);                         \
-        else                                                                            \
-                ((struct _rbtree *)rbt)->cmp.val.d_val = (Val);                         \
-}while(0)
-
 static struct _rbtree NIL = {.attr = {NULL, NULL, NULL, 'B',NULL}, .pdata = NULL}; // *NEVER* free(NIL)!!
-#define get_rbtree_node(cmptype, _Val, data_addr)                                       \
-({                                                                                      \
-        struct _rbtree *name;                                                           \
-        name = NULL;                                                                    \
-        CALLOC_S(struct _rbtree, name, 1, sizeof(struct _rbtree));                      \
-        name->attr.left = name->attr.right = name->attr.parent = (&NIL);                \
-        name->attr.color = 'R';                                                         \
-        name->cmp.type = cmptype;                                                       \
-        if(name->cmp.type == CMP_INT)                                                   \
-                name->cmp.val.i_val = (_Val);                                           \
-        else if(name->cmp.type == CMP_DOUBLE)                                           \
-                name->cmp.val.d_val = (_Val);                                           \
-        else                                                                            \
-                printf("Error : Unknown cmp type %08x\n",(unsigned int) cmptype);       \
-        name->pdata = (void *)data_addr;                                                \
-        FREE_ADDR(name) = name;                                                         \
-        name;                                                                           \
-})
-
-
-#define get_rbtree_node_with_name(name, cmptype, _Val, data_addr)                       \
-({                                                                                      \
-        struct _rbtree *name;                                                           \
-        name = NULL;                                                                    \
-        CALLOC_S(struct _rbtree, name, 1, sizeof(struct _rbtree));                      \
-        name->attr.left = name->attr.right = name->attr.parent = (&NIL);                \
-        name->attr.color = 'R';                                                         \
-        name->cmp.type = cmptype;                                                       \
-        if(name->cmp.type == CMP_INT)                                                   \
-                name->cmp.val.i_val = (_Val);                                           \
-        else if(name->cmp.type == CMP_DOUBLE)                                           \
-                name->cmp.val.d_val = (_Val);                                           \
-        else                                                                            \
-                printf("Error : Unknown cmp type %08x\n",(unsigned int) cmptype);       \
-        name->pdata = (void *)data_addr;                                                \
-        FREE_ADDR(name) = name;                                                         \
-        name;                                                                           \
-})
-/*if data == TRUE,it will free pdata as well*/
-/* if FREE_ADDR(node) == FALSE,then that node is NIL and can not be freed!*/
-#define  DELETE_NODE(node , data)               \
-do                                              \
-{                                               \
-        if(FREE_ADDR(node) != NULL)             \
-        {                                       \
-                if(data == TRUE)                \
-                        free(RBT_DATA(node));   \
-                free((void *)FREE_ADDR(node));  \
-        }                                       \
-}while(0)
 void rbt_insert(struct _rbtree **root, struct _rbtree *node)
 {
         struct _rbtree *cur_node = *root;
@@ -131,61 +37,8 @@ void rbt_insert(struct _rbtree **root, struct _rbtree *node)
         }
 FOUND:
         COLOR(node) = 'R';
-        rbtree_walk(*root, 0);
         rbt_insert_fixup(root,node);
 }
-/********************************************************************************************************/
-/*       |                                                                                      |       */
-/*      (x)                                                                                    (y)      */
-/*      / \                     -----LEFT_ROTATE(x, y)--->                                     / \      */
-/*     a  (y)                   <----RIGHT_ROTATE(y,x)----                                   (x)  c     */
-/*        / \                                                                                / \        */
-/*       b   c                                                                              a   b       */
-/********************************************************************************************************/
-/* 
- * Pay special attention to this cases : if the new y , or new x is new "ROOT",we must manual change the val
- * of root,that why we added the 3rd parameter,root
- */
-#define LEFT_ROTATE(x, y, root)   \
-do                                \
-{                                 \
-        typeof(x) _x = x;         \
-        typeof(y) _y = y;         \
-        if(P(_x) != &NIL)         \
-        {                         \
-        if(LEFT(P(_x)) == _x)     \
-                LEFT(P(_x)) = _y; \
-        else                      \
-                RIGHT(P(_x)) = _y;\
-        }                         \
-        else                      \
-                root = _y;        \
-        P(LEFT(_y)) = _x;         \
-        RIGHT(_x) = LEFT(_y);     \
-        P(_y) = P(_x);            \
-        LEFT(_y) = _x;            \
-        P(_x) = _y;               \
-}while(0)
-
-#define RIGHT_ROTATE(y, x, root)  \
-do                                \
-{       typeof(x) _x = x;         \
-        typeof(y) _y = y;         \
-        if(P(_y) != &NIL)         \
-        {                         \
-        if(LEFT(P(_y)) == _y)     \
-                LEFT(P(_y)) = _x; \
-        else                      \
-                RIGHT(P(_y)) = _x;\
-        }                         \
-        else                      \
-                root = _x;        \
-        P(RIGHT(_x)) = _y;        \
-        LEFT(_y) = RIGHT(_x);     \
-        P(_x) = P(_y);            \
-        RIGHT(_x) = _y;           \
-        P(_y) = _x;               \
-}while(0)                       
 
 void rbt_insert_fixup(struct _rbtree **root,struct _rbtree *node)
 {
@@ -237,27 +90,6 @@ void rbt_insert_fixup(struct _rbtree **root,struct _rbtree *node)
                 n = P(n);
         *root = n;
 }
-#define rbtree_delete_node_with_val(root, val)                          \
-do                                                                      \
-{                                                                       \
-        struct _rbtree *search = root;                                  \
-        while((VAL(search) != val) && (search != &NIL))                 \
-        {                                                               \
-                printf("cmp %g with %d\n",VAL(search),val);             \
-                if(VAL(search) > (double)val)                           \
-                        search = LEFT(search);                          \
-                else if(VAL(search) < (double)val)                      \
-                        search = RIGHT(search);                         \
-                else                                                    \
-                {                                                       \
-                        break;                                          \
-                }                                                       \
-        }                                                               \
-        if(search != &NIL)                                              \
-                rbtree_delete_node(&root, search);                      \
-        else                                                            \
-                printf("Node with val = %g NOT FOUND!\n",val);          \
-}while(0) 
 struct _rbtree *tree_successor(struct _rbtree *node)
 {
         struct _rbtree *p = RIGHT(node);
@@ -292,6 +124,10 @@ void rbtree_delete_node_with_data(struct _rbtree **root,void *data_ptr)
 
                 if((uint32_t)data_ptr == (uint32_t)(RBT_DATA(search)))
                 {
+                printf("FOUND THE NODE WITH DATA %08x, VAL = %g , LEFT = %g , RIGHT = %g\n",
+                        (uint32_t)(RBT_DATA(search)), VAL(search), VAL(LEFT(search)), VAL(RIGHT(search)));
+                rbtree_delete_node(root, search);
+
                         break;
                 }
                 if(LEFT(search) != &NIL)
@@ -304,12 +140,6 @@ void rbtree_delete_node_with_data(struct _rbtree **root,void *data_ptr)
                 }
         }
         DESTORY_QUEUE(node_queue);
-        if(search != NULL)
-        {
-                printf("FOUND THE NODE WITH DATA %08x, VAL = %g , LEFT = %g , RIGHT = %g\n",
-                        (uint32_t)(RBT_DATA(search)), VAL(search), VAL(LEFT(search)), VAL(RIGHT(search)));
-                rbtree_delete_node(root, search);
-        }
 }
 void rbtree_delete_node(struct _rbtree **root, struct _rbtree *node)
 {
@@ -428,20 +258,20 @@ void rbtree_test()
 {
         int datas[] = {12,1,9,2,0,11,7,19,4,15,18,5,14,13,10,16,6,3,8,17}; 
         int i = 0;
-        char *p = "hello 11";
         struct _rbtree *rooot;
         struct _rbtree *rbt_new;
         struct _rbtree *pc;
-        int head , tail;
-        head = tail = 0;
+        char p1[] = "i am data 1";
+        char p2[] = "i am data 2";
+        char p3[] = "i am data 3";
         for( i = 0; i < 20; i++)
         {
                 if(i == 4)
-                        rbt_new = get_rbtree_node(CMP_INT,datas[i],0xdeadbeaf);
+                        rbt_new = get_rbtree_node(CMP_INT,datas[i],p1);
                 else if(i == 11)
-                        rbt_new = get_rbtree_node(CMP_INT,datas[i],0x11111111);
+                        rbt_new = get_rbtree_node(CMP_INT,datas[i],p2);
                 else if(i == 2)
-                        rbt_new = get_rbtree_node(CMP_INT,datas[i],0x22222222);
+                        rbt_new = get_rbtree_node(CMP_INT,datas[i],p3);
                 else
                         rbt_new = get_rbtree_node(CMP_INT,datas[i],NULL);
                 printf("get new node %08x, Val = %g with data %08x\n",(uint32_t)rbt_new,VAL(rbt_new),(uint32_t)(RBT_DATA(rbt_new)));
@@ -485,12 +315,12 @@ void rbtree_test()
                         enqueue(check_list, &(RIGHT(pc)));
                 }
         }
-        rbtree_delete_node_with_data(&rooot, (void *)(0x11111111));
-        rbtree_delete_node_with_data(&rooot, (void *)(0x22222222));
-        rbtree_delete_node_with_data(&rooot, (void *)(0xdeadbeaf));
-        for( i = 0 ; i < 20 ; i++)
-        {
-                rbtree_delete_node_with_val(rooot, datas[i]);
-                rbtree_walk(rooot, 0);
-        }
+        rbtree_delete_node_with_data(&rooot, (void *)p1);
+        rbtree_delete_node_with_data(&rooot, (void *)p2);
+        rbtree_delete_node_with_data(&rooot, (void *)p3);
+//*        for( i = 0 ; i < 20 ; i++)
+//*        {
+//*                rbtree_delete_node_with_val(rooot, datas[i]);
+//*                rbtree_walk(rooot, 0);
+//*        }
 }
--- rbtree/rbtree.h
+++ rbtree/rbtree.h
@@ -26,7 +26,175 @@ struct _rbtree
         struct _cmp cmp;
         void *pdata;
 };
+/*      Use VAL_Int Or VAL_DOUBLE as 'type' parameter      */
+#define VAL_INT                 i_val
+#define VAL_DOUBLE              d_val
 
+/*      do NOT use those two!!    */
+#define CMP_INT 0x4
+#define CMP_DOUBLE 0x12
+
+#define COLOR(rbt)              ((struct _rbtree *)rbt)->attr.color
+#define LEFT(rbt)               ((struct _rbtree *)rbt)->attr.left
+#define RIGHT(rbt)              ((struct _rbtree *)rbt)->attr.right
+#define P(rbt)                  ((struct _rbtree *)rbt)->attr.parent
+#define FREE_ADDR(rbt)          ((struct _rbtree *)rbt)->attr.free_addr
+#define RBT_DATA(rbt)           (((struct _rbtree *)rbt)->pdata)
+#define UNCLE(rbt)              (LEFT(P(P(rbt))) == P(rbt) ? (RIGHT(P(P(rbt)))) : (LEFT(P(P(rbt)))))
+#define RIGHT_UNCLE(rbt)        (RIGHT(P(P(rbt))))
+#define LEFT_UNCLE(rbt)         (LEFT(P(P(rbt))))
+#define IS_UNCLE_LEFT(rbt)      (LEFT(P(P(rbt))) == P(rbt) ? FALSE : TRUE)
+#define IS_PARENT_LEFT(rbt)     (LEFT(P(P(rbt))) == P(rbt) ? TRUE : FALSE)
+#define IS_NODE_LEFT(rbt)       (LEFT(P(rbt)) == rbt ? TRUE : FALSE)
+#define SIBLING(rbt)            ((IS_NODE_LEFT(rbt)) ? RIGHT(P(rbt)) : LEFT(P(rbt)))
+
+#define CMPTYPE(rbt)            ((struct _rbtree *)rbt)->cmp.type
+#define CMP(a,op,c,type)        ((((struct _rbtree *)a)->cmp.val.type op ((struct _rbtree *)c)->cmp.val.type) ? 1 : 0)
+
+/*      Some Value Operations Marcos */
+/*      VAL(brt) use to do the comparision ONLY e.g : if(VAL(rbt1) > VAL(rbt2)) ......  */
+#define VAL(rbt)                ({CMPTYPE(rbt) == CMP_INT ?                             \
+                                ((struct _rbtree *)rbt)->cmp.val.i_val :                \
+                                ((struct _rbtree *)rbt)->cmp.val.d_val;})
+/*      GET_VAL use to assign dst with rbt's value,Use VAL_Int Or VAL_DOUBLE 3rd parameterOnly! */
+#define GET_VAL(dst, rbt,type)      dst = ((struct _rbtree *)rbt)->cmp.val.type
+
+
+#define SET_VAL_WITH_TYPE(rbt, val, type)  ((struct _rbtree *)rbt)->cmp.val.type = (val)
+#define SET_VAL(rbt, Val)                                                               \
+do                                                                                      \
+{                                                                                       \
+        if((CMPTYPE(rbt)) == CMP_INT)                                                   \
+                ((struct _rbtree *)rbt)->cmp.val.i_val = (Val);                         \
+        else                                                                            \
+                ((struct _rbtree *)rbt)->cmp.val.d_val = (Val);                         \
+}while(0)
+
+#define get_rbtree_node(cmptype, _Val, data_addr)                                       \
+({                                                                                      \
+        struct _rbtree *name;                                                           \
+        name = NULL;                                                                    \
+        CALLOC_S(struct _rbtree, name, 1, sizeof(struct _rbtree));                      \
+        name->attr.left = name->attr.right = name->attr.parent = (&NIL);                \
+        name->attr.color = 'R';                                                         \
+        name->cmp.type = cmptype;                                                       \
+        if(name->cmp.type == CMP_INT)                                                   \
+                name->cmp.val.i_val = (_Val);                                           \
+        else if(name->cmp.type == CMP_DOUBLE)                                           \
+                name->cmp.val.d_val = (_Val);                                           \
+        else                                                                            \
+                printf("Error : Unknown cmp type %08x\n",(unsigned int) cmptype);       \
+        name->pdata = (void *)data_addr;                                                \
+        FREE_ADDR(name) = name;                                                         \
+        name;                                                                           \
+})
+
+
+#define get_rbtree_node_with_name(name, cmptype, _Val, data_addr)                       \
+({                                                                                      \
+        struct _rbtree *name;                                                           \
+        name = NULL;                                                                    \
+        CALLOC_S(struct _rbtree, name, 1, sizeof(struct _rbtree));                      \
+        name->attr.left = name->attr.right = name->attr.parent = (&NIL);                \
+        name->attr.color = 'R';                                                         \
+        name->cmp.type = cmptype;                                                       \
+        if(name->cmp.type == CMP_INT)                                                   \
+                name->cmp.val.i_val = (_Val);                                           \
+        else if(name->cmp.type == CMP_DOUBLE)                                           \
+                name->cmp.val.d_val = (_Val);                                           \
+        else                                                                            \
+                printf("Error : Unknown cmp type %08x\n",(unsigned int) cmptype);       \
+        name->pdata = (void *)data_addr;                                                \
+        FREE_ADDR(name) = name;                                                         \
+        name;                                                                           \
+})
+/*if data == TRUE,it will free pdata as well*/
+/* if FREE_ADDR(node) == FALSE,then that node is NIL and can not be freed!*/
+#define  DELETE_NODE(node , data)               \
+do                                              \
+{                                               \
+        if(FREE_ADDR(node) != NULL)             \
+        {                                       \
+                if(data == TRUE)                \
+                        free(RBT_DATA(node));   \
+                free((void *)FREE_ADDR(node));  \
+        }                                       \
+}while(0)
+
+/********************************************************************************************************/
+/*       |                                                                                      |       */
+/*      (x)                                                                                    (y)      */
+/*      / \                     -----LEFT_ROTATE(x, y)--->                                     / \      */
+/*     a  (y)                   <----RIGHT_ROTATE(y,x)----                                   (x)  c     */
+/*        / \                                                                                / \        */
+/*       b   c                                                                              a   b       */
+/********************************************************************************************************/
+/* 
+ * Pay special attention to this cases : if the new y , or new x is new "ROOT",we must manual change the val
+ * of root,that why we added the 3rd parameter,root
+ */
+#define LEFT_ROTATE(x, y, root)   \
+do                                \
+{                                 \
+        typeof(x) _x = x;         \
+        typeof(y) _y = y;         \
+        if(P(_x) != &NIL)         \
+        {                         \
+        if(LEFT(P(_x)) == _x)     \
+                LEFT(P(_x)) = _y; \
+        else                      \
+                RIGHT(P(_x)) = _y;\
+        }                         \
+        else                      \
+                root = _y;        \
+        P(LEFT(_y)) = _x;         \
+        RIGHT(_x) = LEFT(_y);     \
+        P(_y) = P(_x);            \
+        LEFT(_y) = _x;            \
+        P(_x) = _y;               \
+}while(0)
+
+#define RIGHT_ROTATE(y, x, root)  \
+do                                \
+{       typeof(x) _x = x;         \
+        typeof(y) _y = y;         \
+        if(P(_y) != &NIL)         \
+        {                         \
+        if(LEFT(P(_y)) == _y)     \
+                LEFT(P(_y)) = _x; \
+        else                      \
+                RIGHT(P(_y)) = _x;\
+        }                         \
+        else                      \
+                root = _x;        \
+        P(RIGHT(_x)) = _y;        \
+        LEFT(_y) = RIGHT(_x);     \
+        P(_x) = P(_y);            \
+        RIGHT(_x) = _y;           \
+        P(_y) = _x;               \
+}while(0)
+
+#define rbtree_delete_node_with_val(root, val)                          \
+do                                                                      \
+{                                                                       \
+        struct _rbtree *search = root;                                  \
+        while((VAL(search) != val) && (search != &NIL))                 \
+        {                                                               \
+                printf("cmp %g with %d\n",VAL(search),val);             \
+                if(VAL(search) > (double)val)                           \
+                        search = LEFT(search);                          \
+                else if(VAL(search) < (double)val)                      \
+                        search = RIGHT(search);                         \
+                else                                                    \
+                {                                                       \
+                        break;                                          \
+                }                                                       \
+        }                                                               \
+        if(search != &NIL)                                              \
+                rbtree_delete_node(&root, search);                      \
+        else                                                            \
+                printf("Node with val = %g NOT FOUND!\n",val);          \
+}while(0) 
 
 void rbt_insert(struct _rbtree **root, struct _rbtree *node);
 void rbt_insert_fixup(struct _rbtree **root, struct _rbtree *node);
